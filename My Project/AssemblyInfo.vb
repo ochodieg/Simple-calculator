a5232ddcc1d2098e586b498dde1062b89dbb1ea3
Ivan <the>
2020-09-06 23:04:26 -0600

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Simple calculator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Simple calculator")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8a30f1d3-99b8-44b1-ad19-1d23ea3fa93f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Simple Calculator.vb
@@ -0,0 +1,95 @@
+﻿'Ivan Ochoa
+'RCET0265
+'Fall 2020
+'Simple Calculator
+'https://github.com/LuisTorres0419/SimpleCalculator.git
+
+Option Strict On
+Option Explicit On
+Option Compare Text
+
+
+Module Module1
+
+    Sub Main()
+        Dim betterData As Boolean
+        Dim userInput1, userInput2, userRespons As String
+        Dim firstNumber, seconNumber As Integer
+
+        Do
+            Console.WriteLine("press q to Quit")
+
+            Do
+
+                Try
+
+                    Console.WriteLine("Please enter a number")
+
+                    userInput1 = Console.ReadLine()
+
+                    firstNumber = CInt(userInput1)
+
+                    Console.WriteLine("enter a second number")
+
+                    userInput2 = Console.ReadLine()
+
+                    seconNumber = CInt(userInput2)
+
+                    betterData = True
+
+                Catch ex As Exception
+
+                    Console.WriteLine("thats a big stinky no no, my friend")
+
+                    betterData = False
+
+
+                    If userInput1 = "q" Or userInput2 = "q" Then
+
+                        Exit Sub
+
+                    End If
+
+                End Try
+
+            Loop Until betterData = True
+
+            Console.Clear()
+
+            Do
+
+                Console.WriteLine("are we adding, or multiplying? ")
+
+                userRespons = Console.ReadLine()
+
+                If userRespons = "Add" Or userRespons = "+" Then
+
+                    Console.WriteLine("You entered: " & firstNumber & " + " & seconNumber & " that = " &
+                                      firstNumber + seconNumber)
+
+                ElseIf userInput1 = "q" Or userInput2 = "q" Then
+
+                    Exit Sub
+
+                ElseIf userRespons = "multiply" Or userRespons = "*" Then
+
+                    Console.WriteLine("You entered: " & firstNumber & " * " & seconNumber & " that = " &
+                                      firstNumber * seconNumber)
+
+                ElseIf userRespons <> "multiply" Or userRespons <> "*" Or userRespons <> "Add" Or userRespons <>
+                    "+" Then
+
+                    Console.WriteLine("no can do")
+
+                End If
+
+            Loop Until userRespons = "multiply" Or userRespons = "*" Or userRespons = "Add" Or userRespons = "+"
+
+            Console.ReadLine()
+
+            Console.Clear()
+        Loop
+
+    End Sub
+
+End Module
